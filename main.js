ef79a487fc95370790a8e6a9baca09c614b77019
Addbot06 <j.cotibar@gmail.com>
2021-10-18 17:38:45 +0800

    HTML/CSS/Bootstrap

---
+++ main.js
@@ -0,0 +1,6 @@
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
+document.querySelector('#app').innerHTML = `
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
